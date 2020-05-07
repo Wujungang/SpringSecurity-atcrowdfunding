bbfe59fce88e45dc67f8daca8e83da3df52379a3
吴俊钢 <172605831@qq.com>
2020-05-07 18:47:45 +0800

    123

---
+++ atcrowdfunding-common/src/main/java/com/atguigu/atcrowdfunding/util/Datas.java
@@ -0,0 +1,25 @@
+package com.atguigu.atcrowdfunding.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Datas {
+
+	private List<Integer> ids = new ArrayList<Integer>();
+	
+	//private List<User> userList = new ArrayList<Integer>();  //  get/set
+	
+	//private List<Integer> age = new ArrayList<Integer>();
+	//private List<String> name = new ArrayList<String>();
+
+	public List<Integer> getIds() { 
+		return ids;
+	}
+
+	public void setIds(List<Integer> ids) {
+		this.ids = ids;
+	}
+
+	
+	
+}
